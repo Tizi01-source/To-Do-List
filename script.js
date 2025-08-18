217205c91c9325e1145cb28db018868e3cf6b795
Tiziano <costantinitiziano05@gmail.com>
2025-08-18 14:49:13 -0300

    Agregado css y js, y html actualizado

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('formulario')
+const tarea = document.getElementById('tarea')
+const lista = document.getElementById("lista-tareas");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const textoTarea = tarea.value;
+    
+    if (textoTarea !== ""){
+        const li = document.createElement("li");
+        li.textContent = textoTarea;
+
+        const btnBorrar = document.createElement("button");
+        btnBorrar.textContent = "Borrar"
+        btnBorrar.classList.add("btn-borrar");
+
+        btnBorrar.addEventListener("click", function () {
+            lista.removeChild(li);
+        });
+
+        li.appendChild(btnBorrar);
+        lista.appendChild(li);
+
+        tarea.value = "";
+    }
+});
