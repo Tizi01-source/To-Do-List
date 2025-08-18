16ed3f91ddc421453d1ad7ae57dc62458e058a1c
Tiziano <costantinitiziano05@gmail.com>
2025-08-18 15:46:34 -0300

    Agregue hora en que se crea una tarea

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const form = document.getElementById('formulario')
 const tarea = document.getElementById('tarea')
 const lista = document.getElementById("lista-tareas");
-const fondo = document.getElementsByTagName("ul");
 
-
+function capitalizar(texto){
+    if(!texto) return "";
+    return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
+}
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const textoTarea = tarea.value;
+    let textoTarea = tarea.value;
+    if (textoTarea === "") return;
+
+    textoTarea = capitalizar(textoTarea)
     
     if (textoTarea !== ""){
         const li = document.createElement("li");
-        li.textContent = textoTarea;
+        li.classList.add("tarea");
 
-        
+        const textoSpan = document.createElement("span");
+        textoSpan.textContent = textoTarea;
+
+        const textoFecha = document.createElement("span");
+        const ahora = new Date();
+        const hora = ahora.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+        textoFecha.textContent = hora;
+        textoFecha.style.margin = "0 10px";
 
         const btnBorrar = document.createElement("button");
         btnBorrar.textContent = "Borrar"
@@ -24,6 +36,8 @@ form.addEventListener("submit", function (event) {
             lista.removeChild(li);
         });
 
+        li.appendChild(textoSpan);
+        li.appendChild(textoFecha);
         li.appendChild(btnBorrar);
         lista.appendChild(li);
 
